fcaaf1a76f061b9c4fb79872fa2f1c80431f0f9e
quantb <quantb@vng.com.vn>
2021-10-16 20:56:35 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'vietnamese-lang-king';
+export class AppComponent implements OnInit {
+  inputText: string;
+  outputText: string;
+
+  constructor() {
+    this.inputText = '';
+    this.outputText = '';
+  }
+
+  ngOnInit(): void {
+  }
+
+  randomText(): void {
+    this.outputText = '';
+    const text = this.inputText.split('');
+    let randomIndex: number;
+    let textFound: string;
+
+    while (text.length !== 0) {
+      randomIndex = Math.floor(Math.random() * text.length);
+      textFound = text[randomIndex];
+      if (textFound !== ' ') {
+        this.outputText += textFound + '/';
+      }
+      text.splice(randomIndex, 1);
+    }
+  }
 }
